ccf51d8d9c21121eb4d0ca1c498f6eb812adb1ef
orya <orya.sp@gmail.com>
2022-11-28 17:28:12 +0200

    Add total number of items in inventory

--- src/features/products/Products.js
+++ src/features/products/Products.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Product from '../products/Product';
+import { selectStoreItems } from '../inventory/inventorySlice';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import styles from './Products.module.css';
+
 
 const selectItemsIds = state => state.inventory.storeItems.map(item => item.id)
 
-function Products() {
+const Products = () => {
     const storeItemsIds = useSelector(selectItemsIds);
+    const storeItems = useSelector(selectStoreItems);
 
     return (
         <div>
+            <div className={styles.numItems}> Total Items: {storeItems.length}</div>
             <Container>
                 <Row xs={2} md={4}>
                     {storeItemsIds.map((itemId,k) => <Product key={k} itemId={itemId} />)}
